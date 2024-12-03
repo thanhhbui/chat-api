81c505288dd6d3be53ef68b0d90e6436c6dfae5d
thanhhbui <thanhbui1173@gmail.com>
2024-12-03 14:08:50 +0700

    connect mongodb env variables

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,1 +1,31 @@
-// mongodb
+import { env } from '~/config/environment'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let chatDatabaseInstance = null
+
+const mongodoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  await mongodoClientInstance.connect()
+
+  chatDatabaseInstance = mongodoClientInstance.db(env.DATABASE_NAME)
+}
+
+export const GET_DB = () => {
+  if (!chatDatabaseInstance) {
+    throw new Error('Must connect to Database first!')
+  } else {
+    return chatDatabaseInstance
+  }
+}
+
+export const CLOSE_DB = async () => {
+  await mongodoClientInstance.close()
+}
