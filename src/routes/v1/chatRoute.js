196170c6c567ac869d19a0c084f2134f600d514d
thanhhbui <thanhbui1173@gmail.com>
2024-12-20 21:33:02 +0700

    error-handling

--- src/routes/v1/chatRoute.js
+++ src/routes/v1/chatRoute.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { chatValidation } from '~/validations/chatValidation'
+import { chatController } from '~/controllers/chatController'
 
 const Router = express.Router()
 
@@ -8,6 +9,6 @@ Router.route('/')
   .get((req, res) => {
     res.status(StatusCodes.OK).json({ message: 'Note: API get list chat' })
   })
-  .post(chatValidation.createNew)
+  .post(chatValidation.createNew, chatController.createNew)
 
 export const chatRoute = Router
